e681a082fd0dbf5754cf3984ab83abe5204943d2
TaiChunChan2580 <146337482+TaiChunChan2580@users.noreply.github.com>
2023-09-28 15:33:08 +0100

    task 102 sos with comments

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -18,75 +18,75 @@ int main()
     while (buttons.BlueButton == 0);
     
     //Repeat everything "forever" (until the power is removed or the chip is reset)
+    //plays the mosrse code with Modify the code to repeatedly play a major scale (C,D,E,F,G,A,B,C).(task 6) but instead of each not playing for 250ms it follows the morese timing
     while (true)
     {
+        //S Morse code part
         //On for 500ms
-        greenLED = 1;
-        buzz.playTone("C");
+        //dot morse code part
+        greenLED = 1; //on led
+        buzz.playTone("C");// plays the tone C
         wait_us(WAIT_TIME_MS * 150);  //150ms
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 150);
+        greenLED = 0; //off led
+        buzz.rest(); //off buzzer
+        wait_us(WAIT_TIME_MS * 150);  //150ms
 
-        greenLED = 1;
-        buzz.playTone("D");
-        wait_us(WAIT_TIME_MS * 150);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 150);
+        greenLED = 1; //on led
+        buzz.playTone("D");// plays the tone D
+        wait_us(WAIT_TIME_MS * 150);  //150ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 150);  //150ms
 
-        greenLED = 1;
-        buzz.playTone("E");
-        wait_us(WAIT_TIME_MS * 150);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 450);
+        greenLED = 1; //on led
+        buzz.playTone("E");// plays the tone E
+        wait_us(WAIT_TIME_MS * 150);  //150ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 450);  //450ms
 
-        greenLED = 1;
-        buzz.playTone("F");
-        wait_us(WAIT_TIME_MS * 450);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 150);
+        greenLED = 1; //on led
+        buzz.playTone("F");// plays the tone F
+        wait_us(WAIT_TIME_MS * 450);  //450ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 150);  //150ms
 
-        greenLED = 1;
-        buzz.playTone("G");
-        wait_us(WAIT_TIME_MS * 450);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 150);
+        greenLED = 1; //on led
+        buzz.playTone("G");// plays the tone G
+        wait_us(WAIT_TIME_MS * 450);  //450ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 150);  //150ms
 
-        greenLED = 1;
-        buzz.playTone("A", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 450);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 450);
+        greenLED = 1; //on led
+        buzz.playTone("A", Buzzer::HIGHER_OCTAVE);// plays the tone A on a higher octave
+        wait_us(WAIT_TIME_MS * 450);  //450ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 450);  //450ms
 
-        greenLED = 1;
-        buzz.playTone("B", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 150);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 150);
+        greenLED = 1; //on led
+        buzz.playTone("B", Buzzer::HIGHER_OCTAVE);// plays the tone B on a higher octave
+        wait_us(WAIT_TIME_MS * 150);  //150ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 150);  //150ms
 
-        greenLED = 1;
-        buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 150);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 150);
+        greenLED = 1; //on led
+        buzz.playTone("C", Buzzer::HIGHER_OCTAVE);// plays the tone C on a higher octave
+        wait_us(WAIT_TIME_MS * 150);  //150ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 150);  //150ms
 
-        greenLED = 1;
-        buzz.playTone("D", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 150);
-        greenLED = 0;
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 900);
+        greenLED = 1; //on led
+        buzz.playTone("D", Buzzer::HIGHER_OCTAVE);// plays the tone D on a higher octave
+        wait_us(WAIT_TIME_MS * 150);  //150ms
+        greenLED = 0; //off led
+        buzz.rest();//off buzzer
+        wait_us(WAIT_TIME_MS * 900);  //900ms for the next sos sequence
 
-        //Pause
-        //buzz.rest();
-        //wait_us(WAIT_TIME_MS * 1000);
 
     }
 }
