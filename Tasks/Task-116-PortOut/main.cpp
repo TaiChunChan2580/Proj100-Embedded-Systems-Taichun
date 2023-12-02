39f1e0daef68ebd64fbd650300899e80fd582b66
TitusChan2580 <tituschantaichun@gmail.com>
2023-12-02 21:31:37 +0000

    task 116

--- Tasks/Task-116-PortOut/main.cpp
+++ Tasks/Task-116-PortOut/main.cpp
@@ -8,12 +8,16 @@ int main()
     lights = 0;
 
     while (true)
-    {
-        lights = 0b0000000000000100;
+    {   
+        //since red is 0b0000000000000100
+        //yellow is 0b0000000000001000
+        //and green is 0b0000000001000000
+        //therefore
+        lights = 0b0000000000001100;//red and yellow light on
         wait_us(1000000);
-        lights = 0b0000000000001000;
+        lights = 0b0000000001001000;//yellow and green on
         wait_us(1000000);
-        lights = 0b0000000001000000;
+        lights = 0b0000000001000100;//green and red on
         wait_us(1000000);                
     }
 }
