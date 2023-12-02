d2ce601a146e21f568b1be7ea46986f4bd1f4c22
TitusChan2580 <tituschantaichun@gmail.com>
2023-12-02 22:17:01 +0000

    task 120

--- Tasks/Task-120-WhileLoop/main.cpp
+++ Tasks/Task-120-WhileLoop/main.cpp
@@ -11,7 +11,9 @@ int main()
 
     printf("\n\nPress Return to Start\n\n");
 
-    while (hits < 1) {
+    //while (hits < 1) {                               //for task4
+    while ( (hits < 1) || (grade>=100) || (grade<0) ) {//for task4
+
         //Clear out the serial port (keyboard)
         flushInputBuffer();
         //Prompt user
@@ -28,3 +30,14 @@ int main()
     //Loop forever
     while (true);
 }
+
+//task 3
+//50 50a 110 says "scanf found 1 hits" and say Grade entered
+//xx it just repeats "Enter grade (0..100)"
+
+//task 5
+//xzy and 100 just repeats "Enter grade (0..100)"
+//50 says "scanf found 1 hits" and say "Grade entered = 50"
+
+//task 6
+//couldn't find errors
