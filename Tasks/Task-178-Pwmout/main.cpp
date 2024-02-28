ec29b353bbc496f65544ea279161ced6033ac2cc
TitusChan2580 <tituschantaichun@gmail.com>
2024-02-28 17:43:10 +0000

    task 178
    d

--- Tasks/Task-178-Pwmout/main.cpp
+++ Tasks/Task-178-Pwmout/main.cpp
@@ -8,6 +8,12 @@ DigitalOut redLED(TRAF_RED1_PIN);
 LCD_16X2_DISPLAY disp;
 PwmOut dispBackLight(LCD_BKL_PIN);
 
+//Task 4
+DigitalOut led1(LED1);
+
+//Task 5
+AnalogIn ldr(AN_LDR_PIN);
+
 int main()
 {
     //Configure the PWM for the backlight 
@@ -32,21 +38,43 @@ int main()
     Timer tmr;
     tmr.start();
 
+    //Task 6 to iniitate value
+    float u = pot;
+    int count = 0;
+
     while(true) {
         
         //Once every 100ms, re-read the POT and update the duty
         if (tmr.elapsed_time() >= 250ms) {
-            float u = pot;              // Every 250ms, read the pot... 
+            u = pot;              // Every 250ms, read the pot... 
             dispBackLight.write(u);     // ... and update the brightness.
+            led1.write(u);//Task 5
             tmr.reset();
         }
         
-        //Other non-blocking code could go here
         
-    }
+        
+        //Other non-blocking code could go here
 
+        //Task 5 measures the ldr value and prints it
+        unsigned short ldrv = ldr.read_u16();
+        printf("Light Dependant Resistor: %d\n", ldrv);
+        
+        //Task 6
+        if (tmr.elapsed_time() >= 250ms) {
+            count = count + 1;//wait it it counts 4 ticks so 4x of 250ms
+            if (count == 3){
+                disp.cls();
+                disp.locate(0, 0);
+                unsigned short potv = pot.read_u16();// reads the value of the pot
+                disp.printf("Pot value: %X ", potv);//print pot value on lcd screen
+                count = 0;//resets counter so it can repeat the loop
+            }
+        }
+    }
 }
 
-
+//Task 2
+//the change in the lcd brightness is very choppy
 
 
